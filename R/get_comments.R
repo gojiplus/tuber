eca6213ea6402fea077f184030863aa62b60b4e3
Gaurav Sood <gsood07@gmail.com>
2016-01-07 09:55:15 -0500

    supports maxResults

--- R/get_comments.R
+++ R/get_comments.R
@@ -2,6 +2,7 @@
 #'
 #' @param video_id id of the video; required
 #' @param simplify Boolean. Default is TRUE. If TRUE, the function returns a data frame. Else a list with all the information returned.
+#' @param maxResults Default is 20. A numeric. Takes values between 20 and 100.
 #' 
 #' @return Nested named list. The entry \code{items} is a list of comments along with meta information. 
 #' Within each of the \code{items} is an item \code{snippet} which has an item \code{topLevelComment$snippet$textDisplay}
@@ -14,13 +15,14 @@
 #' get_comments(video_id="N708P-A45D0")
 #' }
 
-get_comments <- function (video_id=NULL, simplify=TRUE) {
+get_comments <- function (video_id=NULL, simplify=TRUE, maxResults=20) {
 
 	if (is.null(video_id)) stop("Must specify a video ID")
+	if(maxResults < 20 | maxResults > 100) stop("maxResults only takes a value between 20 and 100")
 
 	yt_check_token()
-
-	querylist <- list(part="snippet", videoId = video_id)
+	
+	querylist <- list(part="snippet", videoId = video_id, maxResults=maxResults)
 	req <- GET("https://www.googleapis.com/youtube/v3/commentThreads", query=querylist, config(token = getOption("google_token")))
 	stop_for_status(req)
 	res <- content(req)
