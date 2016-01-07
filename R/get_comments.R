84bd3ace8c79f966b5573a88fd656bff49cd7cb6
Gaurav Sood <gsood07@gmail.com>
2016-01-07 09:40:17 -0500

    supports return of dataframe

--- R/get_comments.R
+++ R/get_comments.R
@@ -1,18 +1,20 @@
 #' Get Comments On a Video
 #'
 #' @param video_id id of the video; required
+#' @param simplify Boolean. Default is TRUE. If TRUE, the function returns a data frame. Else a list with all the information returned.
+#' 
 #' @return Nested named list. The entry \code{items} is a list of comments along with meta information. 
 #' Within each of the \code{items} is an item \code{snippet} which has an item \code{topLevelComment$snippet$textDisplay}
 #' that contains the actual comment.
 #'  
 #' @export
-#' @references \url{https://console.developers.google.com/project}
+#' @references \url{https://developers.google.com/youtube/v3/docs/}
 #' @examples
 #' \dontrun{
 #' get_comments(video_id="N708P-A45D0")
 #' }
 
-get_comments <- function (video_id=NULL) {
+get_comments <- function (video_id=NULL, simplify=TRUE) {
 
 	if (is.null(video_id)) stop("Must specify a video ID")
 
@@ -23,6 +25,14 @@ get_comments <- function (video_id=NULL) {
 	stop_for_status(req)
 	res <- content(req)
 	
+	if (simplify==TRUE) {
+		simple_res  <- lapply(res$items, function(x) x$snippet$topLevelComment$snippet)
+		simpler_res <- as.data.frame(do.call(rbind, simple_res))
+
+		return(invisible(simpler_res))
+	}
+
 	return(invisible(res))	
+
 }
 
