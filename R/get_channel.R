d220008d595fb3e2ab42096907f7a75ef2481ad7
amrrs <amrrs@outlook.com>
2016-03-22 18:30:13 +0530

    added new line in the code

--- R/get_channel.R
+++ R/get_channel.R
@@ -21,7 +21,7 @@ get_channel <- function (channel_id=NULL) {
     res <- tuber_GET("channels", querylist)
     res1 <- res$items[[1]]$statistics
     res2 <- res$items[[1]]$snippet
-    	cat('Channel Title',res2$title)
+    	cat('Channel Title',res2$title, "\n")
 	cat('No. of Views', res1$viewCount, "\n")
 	cat('No. of Subscribers', res1$subscriberCount, "\n")
 	cat('No. of Videos', res1$videoCount, "\n")
