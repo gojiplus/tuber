511a9187ebfb37a010830ee2a7c6eba67a34480f
***** <gsood07@gmail.com>
2016-06-20 14:32:20 -0400

    added get activities, testing works (or should)

--- R/get_activities.R
+++ R/get_activities.R
@@ -1,39 +1,27 @@
 #' Get Channel Activity
 #' 
+#' Returns a list of channel activity events that match the request criteria. 
 #' 
-#' @param video_id id of the video; required; no default
-#' @param lang  language of the caption; required; default is english ("en")
+#' @param channel_id id of the channel; required; no default
+#' @param part specify which part do you want. It can only be one of the three: contentDetails, id, snippet. Default is snippet.
 #' @param \dots Additional arguments passed to \code{\link{tuber_GET}}.
 #' 
 #' @return captions for the video from one of the first track
 #' @export
-#' @references \url{https://developers.google.com/youtube/v3/docs/captions/list}
+#' @references \url{https://developers.google.com/youtube/v3/docs/activities/list}
 #' @examples
 #' \dontrun{
-#' get_activities(video_id="yJXTXN4xrI8")
+#' get_activities(channel_id="UCRw8bIz2wMLmfgAgWm903cA")
 #' }
 
-get_activities <- function (video_id=NULL, lang="en", ...) {
+get_activities <- function (channel_id=NULL, part="snippet", ...) {
 
-	if (is.null(video_id)) stop("Must specify a video ID")
+	
+	if (is.null(channel_id)) stop("Must specify a channel_id")
 
-	yt_check_token()
+	querylist <- list(part=part, channelId=channel_id)
 
-	# Try getting captions directly
-	req <- GET(paste0("http://video.google.com/timedtext?lang=", lang, "&v=", video_id))
-
-	# If not try other things
-	if (length(content(req))==0) {
-		querylist = list(part="snippet", videoId = video_id)
-		req <- GET("https://www.googleapis.com/youtube/v3/captions", query = querylist, config(token = getOption("google_token")), ...)
-		stop_for_status(req)
-		# Multiple caption tracks possible but for now harvest just the first
-		caption_id <- content(req)$items[[1]]$id
-
-		caption <- GET(paste0("https://www.googleapis.com/youtube/v3/captions/", caption_id), config(token = getOption("google_token")), ...)
-		if(caption$status!=200) stop("Caption Track Either Not Found or Not Accessible.")
-		req <- NA
-	}
-
-	content(req)
+	res <- tuber_GET("activities", querylist, ...)
+ 
+ 	res
 }
---
+++ tests/testthat/test-get-activities.R
@@ -0,0 +1,12 @@
+context("Get Activities")
+
+test_that("get_activities runs successfully", {
+
+  skip_on_cran()
+  
+  google_token <- readRDS("token_file.rds")$google_token
+  options(google_token=google_token)
+
+  get_info <- get_activities(channel_id="UCRw8bIz2wMLmfgAgWm903cA")
+  expect_that(get_info, is_a("list"))
+})
