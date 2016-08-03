83b5b459011243c6fc89e7e982ab88b1e13691fb
***** <gsood07@gmail.com>
2016-08-03 13:51:39 -0400

    update the API to remove list_channel_videos and introduce more generic resources

--- R/list_channel_vids.R
+++ R/list_channel_vids.R
@@ -1,29 +1,25 @@
-#' List All the Videos in a Channel
+#' Returns List of Requested Channel Resources
 #' 
 #' @param channel_name string, channel name, required
+#' @param part a comma separated list of channel resource properties that response will include
+#' string, required, one of the following: auditDetails, brandingSettings, contentDetails, contentOwnerDetails, id, invideoPromotion, localizations, snippet, statistics, status, topicDetails
+#' default is contentDetails
 #' @param \dots Additional arguments passed to \code{\link{tuber_GET}}.
 #' 
-#' @return data.frame with 3 columns: hl (two letter abbreviation), name (of the language), etag
+#' @return list
 #' @export
 #' @references \url{https://developers.google.com/youtube/v3/docs/channels/list}
 #' @examples
 #' \dontrun{
 #' list_channel_videos("latenight")
+#' list_channel_videos("latenight", part="id, contentDetails")
 #' }
 
-list_channel_videos <- function (channel_name=NULL, ...) 
+list_channel_resources <- function (channel_name=NULL, part="contentDetails",...) 
 {
-     querylist <- list(part = "contentDetails", forUsername = channel_name)
+     querylist <- list(part = part, forUsername = channel_name)
      res <- tuber_GET("channels", querylist, ...)
-     resdf <- NA
-     if (length(res$items) != 0) {
-          simple_res <- lapply(res$items, function(x) c(unlist(x$snippet), 
-                                                        etag = x$etag))
-          resdf <- as.data.frame(do.call(rbind, simple_res))
-     }
-     else {
-          resdf <- 0
-     }
-     cat("Total Number of Videos:", length(res$items), "\n")
-     return(invisible(resdf))
+    
+     return(invisible(res))
 }
+
