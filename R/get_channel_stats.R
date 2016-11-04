7ab72eeb8e95bf8d3619db131c9e340fa9a37bd4
***** <gsood07@gmail.com>
2016-11-04 17:57:56 -0400

    error handling for mistyped channel_id + returning nicer list

--- R/get_channel_stats.R
+++ R/get_channel_stats.R
@@ -1,11 +1,17 @@
 #' Get statistics of a Channel
 #'
 #' @param channel_id Character. Id of the channel
-#' 
-#' @return list with 5 elements: viewCount, likeCount, dislikeCount, favoriteCount, commentCount
 #' @param \dots Additional arguments passed to \code{\link{tuber_GET}}.
+#' 
+#' @return nested named list with top element names:
+#' kind, etag, id, snippet (list of details of the channel including title), statistics (list of 5)
+#' 
+#' If the channel_id is mistyped or there is no information, an empty list is returned
+#' 
 #' @export
+#' 
 #' @references \url{https://developers.google.com/youtube/v3/docs/channels/list}
+#' 
 #' @examples
 #' \dontrun{
 #' get_channel_stats(channel_id="UChTJTbr5kf3hYazJZO-euHg")
@@ -14,19 +20,24 @@
 get_channel_stats <- function (channel_id = NULL, ...) {
 
 	if (is.null(channel_id)) stop("Must specify a channel ID")
-
-	yt_check_token()
 	
-	querylist <- list(part="statistics,snippet", id = channel_id)
+	querylist <- list(part = "statistics,snippet", id = channel_id)
     
-    res <- tuber_GET("channels", querylist, ...)
-    res1 <- res$items[[1]]$statistics
-    res2 <- res$items[[1]]$snippet
+    raw_res  <- tuber_GET("channels", querylist, ...)
+
+    if (length(raw_res$items) ==0) { 
+    	cat("No channel stats available. Likely cause: Incorrect channel_id. \n")
+    	return(list())
+    }
+
+    res  <- raw_res$items[[1]] 
+    res1 <- res$statistics
+    res2 <- res$snippet
     
-    cat('Channel Title',res2$title, "\n")
-	cat('No. of Views', res1$viewCount, "\n")
-	cat('No. of Subscribers', res1$subscriberCount, "\n")
-	cat('No. of Videos', res1$videoCount, "\n")
- 
+    cat('Channel Title:', res2$title, "\n")
+	cat('No. of Views:', res1$viewCount, "\n")
+	cat('No. of Subscribers:', res1$subscriberCount, "\n")
+	cat('No. of Videos:', res1$videoCount, "\n")
+ 	
 	res
 }
