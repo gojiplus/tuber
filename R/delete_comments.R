882c575ec6c8ede1f1fe4ea2ddac846a73c1bff4
Gaurav Sood <gsood07@gmail.com>
2017-05-04 08:30:08 -0400

    delete comments and playlists added

---
+++ R/delete_comments.R
@@ -0,0 +1,30 @@
+#' Delete a Particular Comment
+#'  
+#' @param id   String. Required. id of the comment being retrieved
+#' @param \dots Additional arguments passed to \code{\link{tuber_DELETE}}.
+#' 
+#' @return 
+#' 
+#' @references \url{https://developers.google.com/youtube/v3/docs/comments/delete}
+#' 
+#' @export
+#'  
+#' @examples
+#' \dontrun{
+#' 
+#' # Set API token via yt_oauth() first
+#' 
+#' delete_comments(id = "y3ElXcEME3lSISz6izkWVT5GvxjPu8pA")
+#' }
+
+delete_comments <- function (id = NULL, ...) {
+
+  if ( !is.character(id)) {
+    stop("Must specify a valid id.")
+  }
+
+  querylist <- list(id = id)
+  raw_res <- tuber_DELETE("comments", query = querylist, ...)
+
+  raw_res
+}
---
+++ R/delete_playlists.R
@@ -0,0 +1,30 @@
+#' Delete a Playlist
+#'  
+#' @param id   String. Required. id of the playlist that is to be deleted
+#' @param \dots Additional arguments passed to \code{\link{tuber_DELETE}}.
+#' 
+#' @return 
+#' 
+#' @references \url{https://developers.google.com/youtube/v3/docs/playlists/delete}
+#' 
+#' @export
+#'  
+#' @examples
+#' \dontrun{
+#' 
+#' # Set API token via yt_oauth() first
+#' 
+#' delete_playlists(id = "y3ElXcEME3lSISz6izkWVT5GvxjPu8pA")
+#' }
+
+delete_playlists <- function (id = NULL, ...) {
+
+  if ( !is.character(id)) {
+    stop("Must specify a valid id.")
+  }
+
+  querylist <- list(id = id)
+  raw_res <- tuber_DELETE("playlists", query = querylist, ...)
+
+  raw_res
+}
