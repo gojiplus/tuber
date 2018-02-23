8ce65115c7c4a295707612b0a2504b651d3a29a2
muschellij2 <muschellij2@gmail.com>
2018-02-23 17:28:26 -0500

    added the rproj and the list_my_channel

--- R/get_channel_stats.R
+++ R/get_channel_stats.R
@@ -2,21 +2,21 @@
 #'
 #' @param channel_id Character. Id of the channel
 #' @param \dots Additional arguments passed to \code{\link{tuber_GET}}.
-#' 
+#'
 #' @return nested named list with top element names:
 #' \code{kind, etag, id, snippet (list of details of the channel including title), statistics (list of 5)}
-#' 
+#'
 #' If the \code{channel_id} is mistyped or there is no information, an empty list is returned
-#' 
+#'
 #' @export
-#' 
+#'
 #' @references \url{https://developers.google.com/youtube/v3/docs/channels/list}
-#' 
+#'
 #' @examples
 #' \dontrun{
-#' 
+#'
 #' # Set API token via yt_oauth() first
-#' 
+#'
 #' get_channel_stats(channel_id="UCMtFAi84ehTSYSE9XoHefig")
 #' get_channel_stats(channel_id="UCMtFAi84ehTSYSE9Xo") # Incorrect channel ID
 #' }
@@ -31,6 +31,34 @@ get_channel_stats <- function(channel_id = NULL, ...) {
 
   if (length(raw_res$items) == 0) {
     warning("No channel stats available. Likely cause: Incorrect channel_id.\n")
+    return(list())
+  }
+
+  res  <- raw_res$items[[1]]
+  res1 <- res$statistics
+  res2 <- res$snippet
+
+  cat("Channel Title:", res2$title, "\n")
+  cat("No. of Views:", res1$viewCount, "\n")
+  cat("No. of Subscribers:", res1$subscriberCount, "\n")
+  cat("No. of Videos:", res1$videoCount, "\n")
+
+  res
+}
+
+
+#' @rdname get_channel_stats
+#' @export
+list_my_channel <- function(...) {
+
+
+  querylist <- list(part = "snippet,contentDetails,statistics",
+                    mine = "true")
+
+  raw_res  <- tuber_GET("channels", querylist, ...)
+
+  if (length(raw_res$items) == 0) {
+    warning("No channel stats available. Likely cause: No videos.\n")
     return(list())
   }
 
