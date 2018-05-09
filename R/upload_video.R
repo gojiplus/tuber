74d5db4608e530a4522ef6d8becbb301fc3563b0
John Muschelli <muschellij2@gmail.com>
2018-05-09 00:03:21 -0400

    Fixes upload_video Function (#56)
    * added the rproj and the list_my_channel
    
    * - Added the upload_video option
    
    * Upload video now works.
    
    * updated the encoding and the docs
    
    * upaded the package
    
    * upaded the package
    
    * upaded the package
    
    * upaded the package
    
    * upaded the package
    
    * upaded the package
    
    * upaded the package
    
    * upaded the package
    
    * tags now work
    
    * updated docs for tags
    
    * Fixing upload.
    
    * Shit seems broken.

--- R/upload_video.R
+++ R/upload_video.R
@@ -5,31 +5,98 @@
 #' and `title`.  See
 #' \url{https://developers.google.com/youtube/v3/docs/videos#resource} for
 #' other fields.  Coerced to a JSON object
+#' @param status Additional fields to be put into the \code{status} input.
+#' options for `status` are `license` (which should hold:
+#' `creativeCommon`, or `youtube`), `privacyStatus`, `publicStatsViewable`,
+#' `publishAt`.
 #' @param query Fields for `query` in `POST`
-#' @param part The part parameter serves two purposes in this operation.
-#' It identifies the properties that the write operation will set as
-#' well as the properties that the API response will include.
-#' See \url{https://developers.google.com/youtube/v3/docs/videos/insert#usage}
 #' @param ... Additional arguments to send to \code{\link{tuber_POST}} and
 #' therefore \code{\link{POST}}
+#' @param open_url Should the video be opened using \code{\link{browseURL}}
 #'
-#' @return A list of the response object from the \code{POST} and content
+#' @note The information for `status` and `snippet` are at
+#' \url{https://developers.google.com/youtube/v3/docs/videos#resource}
+#' but the subset of these fields to pass in are located at:
+#' \url{https://developers.google.com/youtube/v3/docs/videos/insert}
+#' The `part`` parameter serves two purposes in this operation.
+#' It identifies the properties that the write operation will set, this will be
+#' automatically detected by the names of `body`.
+#' See \url{https://developers.google.com/youtube/v3/docs/videos/insert#usage}
+#' @return A list of the response object from the \code{POST}, content,
+#' and the URL of the uploaded
 #' @export
 #'
 #' @importFrom jsonlite toJSON
+#' @importFrom utils browseURL
 #' @importFrom httr upload_file
+#' @examples
+#' snippet = list(
+#' title = "Test Video",
+#' description = "This is just a random test.",
+#' tags = c("r language", "r programming", "data analysis")
+#' )
+#' status = list(privacyStatus = "private")
 
 upload_video <- function(
   file,
-  snippet = list(),
+  snippet = NULL,
+  status = list(privacyStatus = "public"),
   query = NULL,
-  part = "snippet,status",
+  open_url = FALSE,
   ...
 ) {
+
+
+
+  if ("privacyStatus" %in% names(status)) {
+    p <- status$privacyStatus
+    p <- match.arg(p, choices = c("private", "public", "unlisted"))
+  }
+
+  if ("license" %in% names(status)) {
+    p <- status$license
+    p <- match.arg(p, choices = c("creativeCommon", "youtube"))
+  }
+
+  if ("tags" %in% names(snippet)) {
+    tags <- snippet$tags
+    if (length(tags) == 1) {
+      tags = list(tags)
+    }
+    snippet$tags = tags
+  }
+
+
+
+  metadata <- tempfile()
+  body <- list()
+  if (length(snippet) == 0) {
+    snippet = NULL
+  }
+  if (length(status) == 0) {
+    status = NULL
+  }
+  body$snippet = snippet
+  body$status = status
+
+  part = paste(names(body), collapse = ",")
+
   query <- as.list(query)
   query$part <- part
-  body <- list(snippet = jsonlite::toJSON(snippet),
-              y = httr::upload_file(file))
+
+  body <- jsonlite::toJSON(body, auto_unbox = TRUE)
+  writeLines(body, metadata)
+
+  # body <- list(
+  #   snippet = jsonlite::toJSON(snippet, auto_unbox = auto_unbox),
+  #   status = jsonlite::toJSON(status, auto_unbox = auto_unbox),
+  #   y = httr::upload_file(file))
+
+  body <- list(
+    metadata = upload_file(metadata, type = "application/json; charset=UTF-8"),
+    y = httr::upload_file(file))
+
+
 
   yt_check_token()
   # need diff from regular tuber_POST because of upload/
@@ -37,9 +104,21 @@ upload_video <- function(
                     body = body, query = query,
                     config(token = getOption("google_token")),
                     ...)
-
+  if (httr::status_code(req) > 300)  {
+    print(body)
+    print(paste0("File is: ", metadata))
+    cat(readLines(metadata))
+    cat("\n")
+    print(query)
+    print(httr::content(req)$error)
+    stop("Request was bad")
+  }
   tuber_check(req)
   res <- content(req)
-  
-  list(request = req, content = res)
+  url = paste0("https://www.youtube.com/watch?v=", res$id)
+  if (open_url) {
+    browseURL(url)
+  }
+  list(request = req, content = res,
+       url = url)
 }
