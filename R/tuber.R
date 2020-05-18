bbc87756e2551accff0dc9c7c2ba70fb5f10195b
muschellij2 <muschellij2@gmail.com>
2020-05-18 00:09:48 -0400

    updated the tuber main docs for Roxygen2 warning

--- R/tuber.R
+++ R/tuber.R
@@ -1,4 +1,7 @@
-#' \pkg{tuber} provides access to the YouTube API V3.
+#' @title \pkg{tuber} provides access to the YouTube API V3.
+#'
+#' @description \pkg{tuber} provides access to the YouTube API V3 via
+#' RESTful calls.
 #'
 #' @name tuber
 #' @importFrom httr GET POST DELETE authenticate config stop_for_status upload_file content oauth_endpoints oauth_app oauth2.0_token
