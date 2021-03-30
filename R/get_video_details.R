09e14775e8d56324212326fe734a7449df88e117
Balthasar <balthasar.sager@gmail.com>
2021-03-30 02:31:41 +0200

    helpers for `get_video_details()` #109

--- R/get_video_details.R
+++ R/get_video_details.R
@@ -1,3 +1,56 @@
+# helpers for `get_video_details()`
+conditional_unnest_wider <- function(data_input, var) {
+  if (var %in% names(data_input)) {
+    tidyr::unnest_wider(data_input, var, names_sep = "_")
+  } else {
+    data_input
+  }
+}
+
+
+json_to_df <- function(res) {
+  intermediate <- res %>%
+    tibble::enframe() %>%
+    tidyr::pivot_wider() %>%
+    tidyr::unnest(cols = c(kind, etag)) %>%
+    # reflect level of nesting in column name
+    dplyr::rename(response_kind = kind, response_etag = etag) %>%
+    tidyr::unnest(items) %>%
+    tidyr::unnest_wider(col = items) %>%
+    # reflect level of nesting in column name for those that may not be unique
+    dplyr::rename(items_kind = kind, items_etag = etag) %>%
+    tidyr::unnest_wider(snippet)
+
+  intermediate_2 <- intermediate %>%
+    # fields that may not be available:
+    # live streaming details
+    conditional_unnest_wider(var = "liveStreamingDetails") %>%
+    # region restriction (rental videos)
+    conditional_unnest_wider(var = "regionRestriction") %>%
+    conditional_unnest_wider(var = "regionRestriction_allowed") %>%
+    # statistics
+    conditional_unnest_wider(var = "statistics") %>%
+    # status
+    conditional_unnest_wider(var = "status") %>%
+    # player
+    conditional_unnest_wider(var = "player") %>%
+    # contentDetails
+    conditional_unnest_wider(var = "contentDetails") %>%
+    conditional_unnest_wider(var = "topicDetails") %>%
+    conditional_unnest_wider(var = "localized") %>%
+    conditional_unnest_wider(var = "pageInfo") %>%
+    # thumbnails
+    conditional_unnest_wider(var = "thumbnails") %>%
+    conditional_unnest_wider(var = "thumbnails_default") %>%
+    conditional_unnest_wider(var = "thumbnails_standard") %>%
+    conditional_unnest_wider(var = "thumbnails_medium") %>%
+    conditional_unnest_wider(var = "thumbnails_high") %>%
+    conditional_unnest_wider(var = "thumbnails_maxres")
+
+
+  intermediate_2
+}
+
 #' Get Details of a Video or Videos
 #'
 #' Get details such as when the video was published, the title, description,
