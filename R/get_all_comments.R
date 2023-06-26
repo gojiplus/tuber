80c673a08f178326ab08acc8b253bd64cfa505af
soodoku <gsood07@gmail.com>
2023-06-25 20:12:27 -0700

    Update get_all_comments.R

--- R/get_all_comments.R
+++ R/get_all_comments.R
@@ -24,74 +24,60 @@
 #' }
 
 get_all_comments <- function(video_id = NULL, ...) {
-
   querylist <- list(videoId = video_id, part = "id,replies,snippet", maxResults = 100)
-
   res <- tuber_GET("commentThreads", querylist, ...)
-
   agg_res <- process_page(res)
-
-  page_token  <- res$nextPageToken
-
+  page_token <- res$nextPageToken
+  
   while (is.character(page_token)) {
-
     querylist$pageToken <- page_token
     a_res <- tuber_GET("commentThreads", querylist, ...)
     agg_res <- rbind(agg_res, process_page(a_res), stringsAsFactors = FALSE)
-
-    page_token  <- a_res$nextPageToken
-   }
-
+    page_token <- a_res$nextPageToken
+  }
+  
   agg_res
 }
 
 process_page <- function(res = NULL) {
-
-  simple_res  <- lapply(res$items, function(x) {
-                                     unlist(x$snippet$topLevelComment$snippet)
-                                     }
-                                     )
-
-  agg_res <- map_df(simple_res, bind_rows)
-  agg_res <- cbind(agg_res, id = sapply(res$items, `[[`, "id"),
-                            stringsAsFactors = FALSE)
-
-  agg_res$parentId <- NA
-
-  if (!("moderationStatus" %in% names(agg_res))) {
-    agg_res$moderationStatus <- NA
-  }
-
-  n_replies   <- sapply(res$items, function(x) {
-                                     unlist(x$snippet$totalReplyCount)
-                                     }
-                                     )
-
-  for (i in seq_len(n_replies)) {
-
-    if (n_replies[i] == 1) {
-
-      replies_1  <- lapply(res$items[[i]]$replies$comments,
-                                  function(x) c(unlist(x$snippet), id = x$id))
-      replies_1  <- map_df(replies_1, bind_rows)
-
-      if (nrow(replies_1) > 0 & ! ("moderationStatus" %in% names(replies_1))) {
-        replies_1$moderationStatus <- NA
-      }
-      agg_res    <- rbind(agg_res, replies_1, stringsAsFactors = FALSE)
+  simple_res <- lapply(res$items, function(x) {
+    comment_snippet <- unlist(x$snippet$topLevelComment$snippet)
+    comment_id <- x$id
+    comment_parent_id <- NA
+    comment_moderation_status <- if ("moderationStatus" %in% names(comment_snippet)) {
+      comment_snippet$moderationStatus
+    } else {
+      NA
     }
-
-    if (n_replies[i] > 1) {
-
-      replies_1p  <- lapply(res$items[[i]]$replies$comments,
-                                    function(x) c(unlist(x$snippet), id = x$id))
-      replies_1p  <- map_df(replies_1p, bind_rows)
-
-      if (nrow(replies_1p) > 0 & ! ("moderationStatus" %in% names(replies_1p))) {
-        replies_1p$moderationStatus <- NA
+    
+    comment_data <- c(comment_snippet, id = comment_id, parentId = comment_parent_id, moderationStatus = comment_moderation_status)
+    
+    replies <- x$replies
+    if (!is.null(replies) && "comments" %in% names(replies)) {
+      reply_items <- replies$comments
+      
+      if (!is.null(reply_items) && length(reply_items) > 0) {
+        reply_data <- lapply(reply_items, function(reply) {
+          reply_snippet <- unlist(reply$snippet)
+          reply_id <- reply$id
+          reply_parent_id <- comment_id
+          reply_moderation_status <- if ("moderationStatus" %in% names(reply_snippet)) {
+            reply_snippet$moderationStatus
+          } else {
+            NA
+          }
+          
+          c(reply_snippet, id = reply_id, parentId = reply_parent_id, moderationStatus = reply_moderation_status)
+        })
+        
+        reply_data <- do.call(rbind, reply_data)
+        comment_data <- rbind(comment_data, reply_data)
       }
-      agg_res     <- rbind(agg_res, replies_1p, stringsAsFactors = FALSE)
     }
-  }
+    
+    comment_data
+  })
+  
+  agg_res <- do.call(rbind, simple_res)
   agg_res
 }
