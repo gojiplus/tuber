0defc0db15de2dae19c32b2eaa6fcfabf9e9abca
soodoku <gsood07@gmail.com>
2023-06-25 21:08:10 -0700

    Update list_videocats.R

--- R/list_videocats.R
+++ R/list_videocats.R
@@ -24,45 +24,42 @@
 
 list_videocats <- function(filter = NULL, ...) {
 
-  if (!is.character(filter)) {
-    stop("Specify by a valid filter with a valid region code.")
+  valid_filters <- c("category_id", "region_code")
+  if (!is.character(filter) || !names(filter) %in% valid_filters) {
+    stop("Specify a valid filter with a valid region code.")
   }
 
-  translate_filter   <- c(category_id = "id", region_code = "regionCode")
-  yt_filter_name     <- as.vector(translate_filter[match(names(filter),
-                                                      names(translate_filter))])
-  names(filter)      <- yt_filter_name
+  translate_filter <- c(category_id = "id", region_code = "regionCode")
+  yt_filter_name <- translate_filter[names(filter)]
+  names(filter) <- yt_filter_name
 
-  if (sum(is.na(names(filter)) > 0 )) {
+  if (sum(is.na(names(filter))) > 0) {
     stop("Filter can only have region_code or category_id.")
   }
 
-  querylist <- c(list(part = "snippet"), filter)
+  querylist <- list(part = "snippet", filter)
 
   res <- tuber_GET("videoCategories", querylist, ...)
 
   # Cat total results
   cat("Total Number of Categories:", length(res$items), "\n")
 
-  resdf <- read.table(text = "",
-               col.names = c("region_code", "channelId", "title", "assignable",
-                             "etag", "id"))
+  resdf <- data.frame(region_code = filter$regionCode,
+                      channelId = character(),
+                      title = character(),
+                      assignable = logical(),
+                      etag = character(),
+                      id = character(),
+                      stringsAsFactors = FALSE)
 
   if (length(res$items) > 0) {
+    simple_res <- lapply(res$items, function(x) {
+      c(unlist(x$snippet), etag = x$etag, id = x$id)
+    })
 
-    simple_res  <- lapply(res$items,
-                                   function(x) {
-                                   c(unlist(x$snippet), etag = x$etag,
-                                     id = x$id)
-                                   })
-
-    resdf       <- as.data.frame(cbind(region_code = filter["regionCode"],
-                                 do.call(rbind, simple_res)))
-
-  } else {
-
-    resdf[1, "region_code"] <- filter["regionCode"]
+    resdf <- rbind(resdf, do.call(rbind, simple_res))
   }
 
   resdf
 }
+
