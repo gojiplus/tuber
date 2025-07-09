dc6f0c7118ab7873e22c1ad3fd65b53c7c10a068
gaurav <gsood07@gmail.com>
2025-07-09 12:43:47 -0700

    test: modernize expect_that usage

--- tests/testthat/test-get-details.R
+++ tests/testthat/test-get-details.R
@@ -8,7 +8,7 @@ test_that("get_video_details runs successfully", {
   options(google_token = google_token)
 
   get_info <- get_video_details(video_id = "N708P-A45D0")
-  expect_that(get_info, is_a("list"))
+  expect_true(is.list(get_info))
 })
 
 context("Get Details as Data Frame")
@@ -23,5 +23,6 @@ test_that("get_video_details(as.data.frame = TRUE) runs successfully for multipl
   get_info <- get_video_details(
     video_id = c("LDZX4ooRsWs", "yJXTXN4xrI8"),
     as.data.frame = TRUE)
-  expect_that(get_info, is_a("data.frame"))
+  expect_s3_class(get_info, "data.frame")
+  expect_true(all(c("items_kind", "channelTitle") %in% names(get_info)))
 })
